0afb852d9f19e09b32437fba5c35febd9981853e
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-30 13:43:47 +0300

    major

--- src/functions/socketmultiplayer/index.ts
+++ src/functions/socketmultiplayer/index.ts
@@ -2,20 +2,24 @@ import {io} from "socket.io-client";
 
 const socket = io('http://localhost:3000');
 
+
+const definebehssssss =()=>{
+
 // Listen for game state updates
-socket.on('game-update', (newGameState) => {
-    // Update your game UI based on the new game state
-});
+    socket.on('game-update', (newGameState) => {
+        // Update your game UI based on the new game state
+    });
 
 // Listen for new players joining
-socket.on('new-player', (newPlayer) => {
-    // Handle new player (e.g., add a new sprite at newPlayer.x, newPlayer.y)
-});
+    socket.on('new-player', (newPlayer) => {
+        // Handle new player (e.g., add a new sprite at newPlayer.x, newPlayer.y)
+    });
 
 // Listen for players leaving
-socket.on('player-left', (playerId) => {
-    // Handle player leaving the game
-});
+    socket.on('player-left', (playerId) => {
+        // Handle player leaving the game
+    });
 
+}
 
-export  default  {socket}
+export  default  {definebehssssss}
