cd3ee1605a98575b5dfef1c05f9ff36e7b369875
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-06 13:26:09 +0300

    major

--- src/fp/multi/index.ts
+++ src/fp/multi/index.ts
@@ -1,10 +1,9 @@
-/////////work/////////
-
-
-const startGame = async (gameNum: number, negativeGameSrc: string, positiveGameSrc: string) => {
+const startGame = async (email: string, gameNum: number, negativeGameSrc: string, positiveGameSrc: string) => {
     await window.mnpcmw.functions.wearables.eeg.muse.client.connect();
     await window.mnpcmw.functions.wearables.eeg.muse.startListeningToCalmnessScore();
-    /////////////////  window.mnpcmw.functions.singlePlayer.startRenderingGame(gameSrc);
+    window.mnpcmw.functions.multiPlayer.createSocket(undefined);
+    gameNum ? window.mnpcmw.functions.multiPlayer.joinGame(email, gameNum) : window.mnpcmw.functions.multiPlayer.createGame(email)
+
 }
 
 const resetGame = () => {
